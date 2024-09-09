01c44ff034a8610cd7e83581fb261e5bdb2ef638
Vikas Bhalla <vikas@ellingsentechnologies.com>
2024-09-09 10:19:06 +0530

    Creating and adding sptofy playlist to user account

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ scope = "playlist-modify-private"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientId, client_secret=clientSecret, scope=scope, redirect_uri=redirectURI))
 
 # Get the current user
-results = sp.current_user()
-print(f"Logged in as: {results['id']}")
+user = sp.current_user()
+print(f"Logged in as: {user['id']}")
 
 # Billboard Hot 100 chart scrape script
 getDate = input("Enter the date you want to travel to (format YYYY-MM-DD): ")
@@ -29,11 +29,20 @@ titles = htmlParse.select("li h3#title-of-a-story")  # Update the selector as ne
 all_titles = [title.getText().strip() for title in titles]
 
 print(all_titles)
+all_tracks = []
+
 # Search and print Spotify links for each song
 for title in all_titles:
     result = sp.search(title)
     if result['tracks']['items']:  # Ensure there are results
         print(f"Song: {title}")
-        print(result['tracks']['items'][0]['external_urls']['spotify'])
+        url = result['tracks']['items'][0]['external_urls']['spotify']
+        all_tracks.append(url)
     else:
         print(f"No result found for: {title}")
+
+# create a spotify playlist for user
+playlistCreated = sp.user_playlist_create(user['id'], f"{getDate} Billboard 100", public=False, collaborative=False, description=f"Top 100 Billboard charts from date {getDate}")
+sp.user_playlist_add_tracks(user['id'], playlistCreated['id'], all_tracks, position=None)
+
+print(f"Playlist Id : {playlistCreated['id']}")
